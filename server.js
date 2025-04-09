6c1386c9a116a0e9bb069e87f67c04e34fcdbb19
Aniket Sharma <aniket@Anikets-MacBook-Air.local>
2025-04-09 15:09:26 +0530

    Cache

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
 import express from "express";
+import NodeCache from "node-cache";
 
 const app = express();
 const port = 3000;
 
-const values = {};
+const myCache = new NodeCache({ stdTTL: 3600 }); // 1 hour TTL
+
 app.get('/', (req, res) => {
-//   res.send('Welcome to my server!');
-console.log("Server is live !!");
-res.send("Success")
+  console.log("Server is live !!");
+  res.send("Success");
 });
 
 app.post('/insta', (req, res) => {
-    const {access_token, token_type, expires_in} = req.query;
-    console.log("Query values >>>>>>>>", access_token, token_type, expires_in)
-    values[access_token] = access_token;
-    values[token_type] = token_type;
-    values[expires_in] = expires_in;
-    console.log("Post ")
-    res.send("Access tokens saved.")
-})
+  const { access_token, token_type, expires_in } = req.query;
 
-app.get('/getTokens',(req,res)=>{
-    console.log("Get tokens api hit >>");
-    res.json({
-        "access_token" : values[access_token],
-        "token_type" : values[token_type],
-        "expires_in" : values[expires_in]
-    })
-})
+  console.log("Query values >>>>>>>>", access_token, token_type, expires_in);
+
+  myCache.set("access_token", access_token);
+  myCache.set("token_type", token_type);
+  myCache.set("expires_in", expires_in);
+
+  console.log("Tokens saved to cache");
+  res.send("Access tokens saved.");
+});
+
+app.get('/getTokens', (req, res) => {
+  console.log("Get tokens API hit >>");
+
+  res.json({
+    access_token: myCache.get("access_token"),
+    token_type: myCache.get("token_type"),
+    expires_in: myCache.get("expires_in"),
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
